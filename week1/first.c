478e8dbb3394d77f04813d6c3ba472100ec9f53f
rickyrattlesnake <ricky.ratnayake@gmail.com>
2017-07-22 22:25:04 +1000

    testing

---
+++ week1/first.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include "second.h"
+
+#define MY_VARIABLE 10
+
+int main (void) {
+    printf("my variable : %d \n", MY_VARIABLE);
+
+    printf("multiplier: %d\n", multiplier(2));
+    
+    printf("Hello World!\n");
+
+    return 0;
+}
+
+
